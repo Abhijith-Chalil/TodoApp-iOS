b09b716f50a7ed5789c4a9edf2546464437c453a
Abhijith C <abhijithchalilabc@gmail.com>
2024-10-23 21:10:06 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Abhijith Chalil on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
