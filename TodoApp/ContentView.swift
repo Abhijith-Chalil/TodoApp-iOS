e862641dd07055691325ee475aee2f906696214e
Abhijith C <abhijithchalilabc@gmail.com>
2024-11-07 20:46:00 +0530

    todo app completed

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \Todo.isCompleted) private var toDos: [Todo]
+    @State private var isAlertShowing = false
+    @State private var toDoTitle = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(toDos) {toDo in
+                    HStack {
+                        Button{
+                            toDo.isCompleted.toggle()
+                        }label: {
+                            Image(systemName: toDo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        }
+                        Text(toDo.title)
+                    }
+                }.onDelete(perform: deleteTodos)
+            }.navigationTitle("ToDo App").toolbar {
+                Button(action: {
+                    isAlertShowing.toggle()
+                }, label: {
+                    Image(systemName: "plus.circle")
+                })
+            }.alert("Add Todo", isPresented: $isAlertShowing, actions: {
+                TextField("Enter Todo", text: $toDoTitle)
+                Button{
+                    modelContext.insert(Todo(title: toDoTitle, isCompleted: false))
+                    
+                }label: {
+                    Text("Add")
+                }
+            }).overlay {
+                if toDos.isEmpty{
+                    ContentUnavailableView("Nothing to do here", systemImage: "checkmark.circle.fill")
+                }}
         }
-        .padding()
+    }
+    
+    func deleteTodos(_ indexSet: IndexSet){
+        for index in indexSet {
+            let toDo = toDos[index]
+            modelContext.delete(toDo)
+        }
     }
 }
 
--- TodoApp/TodoAppApp.swift
+++ TodoApp/TodoAppApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct TodoAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.modelContainer(for: Todo.self)
     }
 }
+
+
+@Model class Todo {
+    var title: String
+    var isCompleted: Bool
+    
+    init(title: String, isCompleted: Bool) {
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+}
+
+
+extension Bool: Comparable {
+    public static func <(lhs: Self, rhs: Self)-> Bool {
+        !lhs && rhs
+    }
+}
+
